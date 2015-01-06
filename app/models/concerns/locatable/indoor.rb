ec3fa040970e80a829d6ae8e487866b3e546d192
Tao Wang <moonavw@gmail.com>
2015-01-06 17:23:24 +0800

    - fix indoor's geo index

--- app/models/concerns/locatable/indoor.rb
+++ app/models/concerns/locatable/indoor.rb
@@ -6,10 +6,12 @@ module Locatable
       include Mongoid::Document
       include Mongoid::Geospatial
 
-      field :position, type: Mongoid::Geospatial::Point, spatial: true
+      field :position, type: Mongoid::Geospatial::Point
       field :radius, type: Float
       field :floor, type: Integer
       field :building, type: String
+
+      spatial_index :position, {min: 0, max: 1000000}
     end
   end
 end
