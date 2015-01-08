b1fadfd50811c169c181978ca37d6a87a9cebe62
Tao Wang <moonavw@gmail.com>
2015-01-08 17:16:42 +0800

    - make shop able to know outdoor location from mall

--- app/serializers/shop_serializer.rb
+++ app/serializers/shop_serializer.rb
@@ -1,3 +1,3 @@
 class ShopSerializer < ActiveModel::Serializer
-  attributes :id, :name, :logo, :no, :area, :floor, :building_uuid
+  attributes :id, :name, :logo, :no, :area, :floor, :building_uuid, :location
 end
