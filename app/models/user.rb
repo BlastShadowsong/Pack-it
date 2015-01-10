ffd096ebd9da3242d2f7b50689a100efb5db12d3
Tao Wang <moonavw@gmail.com>
2015-01-11 00:55:35 +0800

    - apply action mailer config - apply async mailing for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
+  devise :database_authenticatable, :async,
+         :registerable, :recoverable, :rememberable, :trackable, :validatable,
          :authentication_keys => [:login]
 
   ## Database authenticatable
@@ -64,8 +64,7 @@ class User
   attr_accessor :login
 
   def name
-    self.email unless self.email.blank?
-    self.tel unless self.tel.blank?
+    (self.email unless self.email.blank?) || (self.tel unless self.tel.blank?)
   end
 
   def to_key
