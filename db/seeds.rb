9be121e44a5869b50b64635fbaa834c46d390b4a
Tao Wang <moonavw@gmail.com>
2015-01-12 09:36:28 +0800

    - only seed admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,3 @@ puts 'DEFAULT USERS'
 
 admin = User.create! :role => :admin, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 puts '- admin: ' << admin.email
-
-user = User.create! :role => :user, :email => ENV['USER_EMAIL'].dup, :password => ENV['USER_PASSWORD'].dup, :password_confirmation => ENV['USER_PASSWORD'].dup
-puts '- user: ' << user.email
