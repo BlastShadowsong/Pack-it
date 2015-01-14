46083459f7458600c914666e863696ee36c7f370
Tao Wang <moonavw@gmail.com>
2015-01-14 18:05:30 +0800

    - comment out the wrong definition of user signed on event

--- app/models/user_signed_on_event.rb
+++ app/models/user_signed_on_event.rb
@@ -1,9 +1,10 @@
 class UserSignedOnEvent < Event
   include Mongoid::Document
 
-  embeds_one :outdoor_position, as: :locatable
-  embeds_one :indoor_position, as: :locatable
-  accepts_nested_attributes_for :outdoor_position, :indoor_position
+  # FIXME: use new type of position/location
+  # embeds_one :outdoor_position, as: :locatable
+  # embeds_one :indoor_position, as: :locatable
+  # accepts_nested_attributes_for :outdoor_position, :indoor_position
 
   belongs_to :user
 end
