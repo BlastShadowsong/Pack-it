a6aaafcef8f92c092597bacd5224c9b75be6cf67
Tao Wang <moonavw@gmail.com>
2015-01-19 21:58:30 +0800

    - improve the query object: accept json string from query string in http get

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,26 @@
 module ApplicationHelper
   ## query object
-  # criteria[field][operator]=value
-  # sort[field]=asc|desc
   # see: http://martinfowler.com/eaaCatalog/queryObject.html
+  # e.g.
+  #   criteria[field]=value
+  #   criteria[field][operator]=value
+  #   criteria={"field":value"}
+  #   criteria={"field":{"operator":"value"}}
+  #   criteria={"and|or":[{"field":{"operator":"value"}},{"field":{"operator":"value"}}]}
+  #   criteria={"and|or":[{"field":value"},{"field":value"}]}
+  #   sort[field]=asc|desc
+  #   sort={field:"asc|desc"}
   def query(queryable, criteria = params[:criteria], sort = params[:sort])
-    queryable = queryable.where(criteria) if criteria
-    queryable = queryable.order_by(sort) if sort
+    if criteria
+      criteria = JSON.parse(criteria) if criteria.is_a?(String)
+      queryable = queryable.where(criteria)
+    end
+
+    if sort
+      sort = JSON.parse(sort) if sort.is_a?(String)
+      queryable = queryable.order_by(sort)
+    end
+
     queryable
   end
 end
