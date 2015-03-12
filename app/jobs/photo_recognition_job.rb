6d88dc2e49c307d8b72428031545269b2d807dbf
BlastShadowsong <lisiyuanwelcome@163.com>
2015-03-12 12:16:37 +0800

    - Add python: 25th try, finally~

--- app/jobs/photo_recognition_job.rb
+++ app/jobs/photo_recognition_job.rb
@@ -7,19 +7,23 @@ class PhotoRecognitionJob < ActiveJob::Base
     photo = File.new("photo.png","r+")
     photo.syswrite(problem.picture.read)
     photo.close
-    result = exec("python RF_script.py ./photo.png")
+    result = `python RF_script.py ./photo.png`
 
-    if result == 1
-      problem.set(tag: "54f6bbf5695a390e79110000")
-    elsif result == 2
-      problem.set(tag: "54f6b970695a390e79090000")
-    elsif result == 3
-      problem.set(tag: "54f6b97a695a390e790b0000")
-    elsif result == 4
-      problem.set(tag: "54f6bbec695a390e790f0000")
-    elsif result == 5
-      problem.set(tag: "54f6bbe3695a390e790d0000")
+    if result[0] == "1"
+      self.set(tag: "54f6bbf5695a390e79110000")
+    elsif result[0] == "2"
+      self.set(tag: "54f6b970695a390e79090000")
+    elsif result[0] == "3"
+      self.set(tag: "54f6b97a695a390e790b0000")
+    elsif result[0] == "4"
+      self.set(tag: "54f6bbec695a390e790f0000")
+    elsif result[0] == "5"
+      self.set(tag: "54f6bbe3695a390e790d0000")
     end
 
+    # distribution
+    DistributeProblemJob.perform_later(problem.id.to_s)
+    # schedule a job to close itself at deadline
+    CloseProblemJob.set(wait: self.duration.minutes).perform_later(problem.id.to_s)
   end
 end
--- app/models/problem.rb
+++ app/models/problem.rb
@@ -187,34 +187,7 @@ class Problem
     #   self.judge_rank
 
     # photo classification
-
-    photo = File.new("photo.png", "r+")
-    photo.syswrite(self.picture.read)
-    photo.close
-    result = `python RF_script.py ./photo.png`
-
-    puts result
-    puts "Length: #{result.length}"
-
-
-    if result[0] == "1"
-      self.set(tag: "54f6bbf5695a390e79110000")
-      puts 1
-    elsif result[0] == "2"
-      self.set(tag: "54f6b970695a390e79090000")
-      puts 2
-    elsif result[0] == "3"
-      self.set(tag: "54f6b97a695a390e790b0000")
-      puts 3
-    elsif result[0] == "4"
-      self.set(tag: "54f6bbec695a390e790f0000")
-      puts 4
-    elsif result[0] == "5"
-      self.set(tag: "54f6bbe3695a390e790d0000")
-      puts 5
-    end
-
-
+    PhotoRecognitionJob.perform_later(self.id.to_s)
     # distribution
     # DistributeProblemJob.perform_later(self.id.to_s)
     # schedule a job to close itself at deadline
