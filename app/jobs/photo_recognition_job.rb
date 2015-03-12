d36da343c0a2a2c0f52934ba6c998cf781ddbc76
BlastShadowsong <lisiyuanwelcome@163.com>
2015-03-12 12:20:36 +0800

    - Add python: 26th try   sorry for a mistake

--- app/jobs/photo_recognition_job.rb
+++ app/jobs/photo_recognition_job.rb
@@ -4,21 +4,21 @@ class PhotoRecognitionJob < ActiveJob::Base
   def perform(problem_id)
     problem = Problem.find(problem_id)
 
-    photo = File.new("photo.png","r+")
+    photo = File.new("photo.png", "r+")
     photo.syswrite(problem.picture.read)
     photo.close
     result = `python RF_script.py ./photo.png`
 
     if result[0] == "1"
-      self.set(tag: "54f6bbf5695a390e79110000")
+      problem.set(tag: "54f6bbf5695a390e79110000")
     elsif result[0] == "2"
-      self.set(tag: "54f6b970695a390e79090000")
+      problem.set(tag: "54f6b970695a390e79090000")
     elsif result[0] == "3"
-      self.set(tag: "54f6b97a695a390e790b0000")
+      problem.set(tag: "54f6b97a695a390e790b0000")
     elsif result[0] == "4"
-      self.set(tag: "54f6bbec695a390e790f0000")
+      problem.set(tag: "54f6bbec695a390e790f0000")
     elsif result[0] == "5"
-      self.set(tag: "54f6bbe3695a390e790d0000")
+      problem.set(tag: "54f6bbe3695a390e790d0000")
     end
 
     # distribution
